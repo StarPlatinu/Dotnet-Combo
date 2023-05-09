8be5854c8c9d182de220f67f0474cb7b0cc6ca2d
StarPlatinu <thanhvp1509@gmail.com>
2023-05-09 21:52:07 +0700

    delegate

---
+++ C# Nang Cao/Delegate/delegate/FuncAction/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+//Func là mẫu delegate có kiểu trả về.
+namespace func_action
+{
+    class FuncAction
+    {
+        static int Sum(int x,int y)
+        {
+            return x + y;
+        }
+
+        public static void TestFunc(int x, int y)
+        {
+            Func<int, int, int> calculate;
+            calculate = Sum;
+
+            var result = calculate(x, y);
+            Console.WriteLine(result);
+        }
+
+        public static void Success(string s)
+        {
+         Console.ForegroundColor = ConsoleColor.Green;
+         Console.WriteLine(string.Format("Success: {0}", s));
+         Console.ResetColor();
+        }
+
+        public static void Warn(string s)
+        {Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(string.Format("Warn: {0}", s));
+            Console.ResetColor();
+
+        }
+
+        static void Sum2(int a,int b, Action callback)
+        {
+            int c = a + b;
+            callback?.DynamicInvoke(c.ToString());
+
+        }
+
+        public static void Main(string[] args) { 
+            TestFunc(5,7);
+
+            Action<string> showLog = null;
+
+            showLog += Success;
+            showLog += Warn;
+            showLog += Success;
+
+            showLog("Test");
+
+            //Sum2(5, 6, (x) => Console.WriteLine($"Tổng hai số là: {x}"));
+            //Sum2(1, 3, Success);
+        }
+    }
+}
---
+++ C# Nang Cao/Delegate/delegate/delegate/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+
+//delegate bạn có thể gán vào nó một, nhiều hàm (phương thức) có sự tương thích về tham số,
+//kiểu trả về, sau đó dùng nó để gọi hàm (giống con trỏ trong C++)
+namespace cs_delegate
+{
+    public class Logs
+    {
+        public delegate void showLog(string message);
+
+        static public void Info(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(string.Format("Info: {0}", s));
+            Console.ResetColor();
+        }
+        static public void Warning(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(string.Format("Warning : {0}", s));
+            Console.ResetColor();
+        }
+
+        public  void TestSgowLog()
+        {
+            showLog showLog;
+            showLog = Info;
+            showLog("Warning");
+
+            showLog = Warning;
+            showLog("Warning");
+
+            Console.WriteLine("-------------------------");
+
+            //Gán nhiều phương thức vào delegate
+            showLog = null;
+            showLog += Warning;
+            showLog += Info;
+            showLog += Warning;
+
+            //showLog("Test Alert"); 
+            showLog?.Invoke("Test Log"); //if (showLog != null) showLog("Mgs")
+            Console.WriteLine("--------------------------");
+            showLog += (x) => Console.WriteLine(string.Format("====>{0}<===", x));
+            showLog?.Invoke("Test Log");
+        }
+
+    }
+    public class Program
+    {
+      
+        static public void Main(string[] args)
+        {
+            Logs logs = new Logs();
+            logs.TestSgowLog();
+        }
+    }
+}
