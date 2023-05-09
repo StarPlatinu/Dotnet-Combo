78fdadb299cb515bb2cafb8011ff15717ad54493
StarPlatinu <thanhvp1509@gmail.com>
2023-05-09 22:44:45 +0700

    lambda

---
+++ C# Nang Cao/Lambda/Lambda/Lambda/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Lambda
+{
+    class Lambda_Method
+    {
+        public delegate int Sum(int a, int b);
+        public static void Main(string[] args)
+        {
+            Sum sum = (int x, int y) =>
+            {
+                return x + y;
+            };
+            int kq = sum(1, 2);
+            Console.WriteLine(kq);
+
+            Console.WriteLine("------------------");
+            //Lambda cho Func
+            Func<int, int, int> sum1 = (x, y) => x + y;
+           
+            int kq1 = sum1(1, 22);
+            
+
+            //Lambsa cho Action
+            Action<int> alert = (int v1) =>
+            {
+                Console.WriteLine(v1);
+            };
+
+
+            alert(kq1);
+        }
+    }
+}
